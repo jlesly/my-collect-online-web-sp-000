31a843e1f44eb773cd5409ecdca4100e1dc1e7cf
Lesly Juarez <leslyj2@gmail.com>
2020-05-01 14:13:14 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,15 @@
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
+
+
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
+
+
+my_collect(array) {|name| name.split(" ").first}
